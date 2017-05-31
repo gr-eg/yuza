e2b745f36d4cb6855596bd29475de9f2afd6bfe3
Greg Woodcock <pr1sm.g@gmail.com>
2017-05-31 21:46:10 +0100

    Allow nil on password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, :email, :password, presence: true
-  validates :password, length: { minimum: 8 }
+  validates :username, :email, presence: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
 
   has_secure_password
 end
